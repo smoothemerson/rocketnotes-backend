c722844a6a046e139345e4677e94b9972193e143
emerson <emerson8096@gmail.com>
2024-06-20 09:53:35 -0300

    fix deployment

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 require("express-async-errors")
 require("dotenv/config")
 
-import { connection as knexdb } from './database/knex/index.js'
-
 const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
 const uploadConfig = require("./configs/upload")
@@ -12,9 +10,6 @@ const express = require("express");
 const routes = require("./routes")
 
 migrationsRun()
-
-migrate();
-knexdb.migrate.latest()
 
 const app = express();
 app.use(cors())
