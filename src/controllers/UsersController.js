75226b30d105e90d520d0a6b68d6137b0cc58836
emerson <emerson8096@gmail.com>
2024-07-11 11:26:49 -0300

    using dependencies inversion

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -2,24 +2,17 @@ const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError")
 
 const UserRepository = require("../repositories/UserRepository")
-
+const UserCreateService = require("../services/UserCreateService")
 const sqliteConnection = require("../database/sqlite")
 
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body
 
-    const userRepository = new UserRepository();
+    const userRepository = new UserRepository()
+    const userCreateService = new UserCreateService(userRepository)
 
-    const checkUserExist = await userRepository.findByEmail(email);
-
-    if (checkUserExist) {
-      throw new AppError("Este e-mail já está em uso.")
-    }
-
-    const hashedPassword = await hash(password, 8)
-
-    await userRepository.create({ name, email, password: hashedPassword })
+    await userCreateService.execute({ name, email, password })
 
     return response.status(201).json()
   }
